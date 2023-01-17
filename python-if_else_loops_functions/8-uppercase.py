9394caecbe4ccfbab6677b15413a0854f8b3c36c
jgnacio <jgnaciogomez@gmail.com>
2023-01-17 20:36:34 -0300

    Fix doesn't contain: [".format("]

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_string = "" 
     for i in range(len(str)):
         if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
-            new_string += chr(ord(str[i]) - 32)
+            print("{}".format(chr(ord(str[i]) - 32)), end='')
         else:
-            new_string += str[i]
-    print(new_string)
+            print("{}".format(str[i]), end='')
+    print()
