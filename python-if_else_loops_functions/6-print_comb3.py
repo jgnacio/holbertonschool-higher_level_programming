670438c680196417af51d7d16366db140425dcfc
jgnacio <jgnaciogomez406@gmail.com>
2023-01-17 09:42:03 -0800

    E231 missing whitespace after ','

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for left in range(8):
-    for right in range(1,10):
+    for right in range(1, 10):
         if (left < right):
             print("{}{}".format(left, right), end=", ")
 print(89)
