b08ffde574cf5c641b090b884c0ebd8c99e08409
jgnacio <jgnaciogomez@gmail.com>
2023-01-17 20:48:19 -0300

    Rewrite script with variable that contain ascii

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
-            print("{}".format(chr(ord(str[i]) - 32)), end='')
-        else:
-            print(str[i], end='')
-    print()
+        c_value = ord(str[i])
+        if (c_value >= 97 and c_value <= 122):
+            c_value -= 32
+        print("{}".format(char(c_value)), end='')
+    print("")
