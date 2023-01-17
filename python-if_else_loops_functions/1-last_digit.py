c19c8b5aef23532e3192548067c7d87d74430d30
jgnacio <jgnaciogomez406@gmail.com>
2023-01-17 06:29:18 -0800

    script that get the last digit of a number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number
+if last < 0:
+    last *= -1
+    last %= 10
+    last *= -1
+else:
+    last %= 10
+print(f"Last digit of {number} is {last} and ", end="")
+
+if last == 0:
+    print("and is 0")
+elif last > 5:
+    print("and is greater than 5")
+elif last < 6 != 0:
+    print("and is less than 6 and not 0")
