7ac7c01def410ed9b848a8932940091866857020
jgnacio <jgnaciogomez406@gmail.com>
2023-01-17 09:40:54 -0800

    fix output whit comma

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for left in range(10):
+for left in range(8):
     for right in range(1,10):
         if (left < right):
             print("{}{}".format(left, right), end=", ")
+print(89)
