3de7628d9df623a58e12ab042a03022dca490723
jgnacio <jgnaciogomez406@gmail.com>
2023-01-18 06:35:12 -0800

    Fix plural cases output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    len = len(sys.argv)
-    print("{} arguments:".format(len - 1))
-    if len > 0:
-        for i in range(1, len):
-            print("{0}: {1}".format(i, sys.argv[i]))
+    len = len(sys.argv) - 1
+    if len == 0:
+        print("{} arguments.".format(len))
+    elif len == 1:
+        print("{} argument:".format(len))
     else:
-        print(".")
+        print("{} arguments:".format(len))
+    if len > 0:
+        for i in range(1, len + 1):
+            print("{0}: {1}".format(i, sys.argv[i]))
