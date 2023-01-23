4c9fe6884782f94e0b027a9e37bae27451e24a11
jgnacio <jgnaciogomez@gmail.com>
2023-01-23 11:10:28 -0300

    Blank space at the end of the output

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j < len(matrix[i]) - 1:
+                print(" ", end="")
         print()
