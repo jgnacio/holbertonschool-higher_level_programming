dbe405acc534e2950fec0d5c3d0596efdba34272
jgnacio <jgnaciogomez@gmail.com>
2023-01-23 11:44:30 -0300

    Fix case: negative numbers

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    max_int = 0
+    max_int = my_list[0]
     for i in my_list:
         if i > max_int:
             max_int = i
