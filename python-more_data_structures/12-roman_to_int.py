b31f3ddd207c96f2693ba95bd93b867b0bb4fda7
jgnacio <jgnaciogomez@gmail.com>
2023-01-24 13:53:32 -0300

    E128 continuation line under-indented for visua...

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     roman_string = roman_string.strip()
     roman_string = roman_string.upper()
     roman = {"CM": 900, "CD": 400, "XC": 90, "XL": 40, "IX": 9, "IV": 4,
-            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+             "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_int = 0
     i = 0
     while i < len(roman_string):
