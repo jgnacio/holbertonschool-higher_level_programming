46f8319ae24bf5b099439c3302156fdeddff8ef0
jgnacio <jgnaciogomez@gmail.com>
2023-01-24 14:36:10 -0300

    Rename variables for PEP8 max char per line

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda m_list:
-                    list(map(lambda sn: sn ** 2, m_list)), matrix))
+    return list(map(lambda l: list(map(lambda s: s ** 2, l)), matrix))
