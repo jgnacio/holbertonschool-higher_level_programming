7fdf29b422b2c4fb9143187ac04f708d6b59dbc5
jgnacio <jgnaciogomez@gmail.com>
2023-01-24 13:52:07 -0300

    Fix  case non len multiples of 2 and, and PEP8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != type(""):
+    if not isinstance(roman_string, str):
         return 0
     roman_string = roman_string.strip()
     roman_string = roman_string.upper()
-    roman = {"CM": 900,"CD": 400, "XC": 90, "XL": 40,"IX": 9,"IV": 4,
-    "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman = {"CM": 900, "CD": 400, "XC": 90, "XL": 40, "IX": 9, "IV": 4,
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_int = 0
-    for i in range(0, len(roman_string), 2):
+    i = 0
+    while i < len(roman_string):
         check = roman_string[i:i + 2]
         if len(check) == 2:
             if check in roman:
                 roman_int += roman[check]
-            elif roman_string[i] in roman and roman_string[i + 1] in roman:
-                roman_int += roman[roman_string[i]] + roman[roman_string[i + 1]]
+                i += 2
+            elif roman_string[i] in roman:
+                roman_int += roman[roman_string[i]]
+                i += 1
         else:
             if roman_string[i] in roman:
                 roman_int += roman[roman_string[i]]
+                i += 1
     return roman_int
