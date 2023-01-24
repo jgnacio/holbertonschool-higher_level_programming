9d9c2accf6816b06e6a8a78742dc6b92f4a6432b
jgnacio <jgnaciogomez@gmail.com>
2023-01-24 14:34:23 -0300

    E501 line too long (82 > 79 characters)

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda m_list: list(map(lambda sn: sn ** 2, m_list)), matrix))
+    return list(map(lambda m_list:
+                    list(map(lambda sn: sn ** 2, m_list)), matrix))
