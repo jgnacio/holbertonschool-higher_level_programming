919dc2649f85dfe7d882346a5ad89d3371d929b7
jgnacio <jgnaciogomez@gmail.com>
2023-01-24 10:00:20 -0300

    Fix case: 'my_list = []'

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if my_list == []:
+        return 0
     result = [my_list[0]]
     for i in range(1, len(my_list)):
         if my_list[i] not in result:
