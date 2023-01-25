786bb8a23f79b962b497047b9a4c1f3177f2a571
jgnacio <jgnaciogomez@gmail.com>
2023-01-25 11:35:30 -0300

    Rewrite the function and return the index

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x + 1):
+    i = 0
+    while i < x:
         try:
             print(my_list[i], end="")
-        except IndexError :
+        except IndexError:
             print()
             return i
+        else:
+            i += 1
     print()
     return i
