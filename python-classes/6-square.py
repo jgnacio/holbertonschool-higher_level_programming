4a64919ff107611f23e87393cfcbedc77a0918e7
jgnacio <jgnaciogomez@gmail.com>
2023-01-30 14:19:38 -0300

    add: handle exception for IndexError

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,6 +10,8 @@ Created on Mon Jan 30 13:32:00 2023.
 class Square():
     """Represent a Square with size."""
 
+    TUPLEERROR = "position must be a tuple of 2 positive integers"
+
     def __init__(self, size=0, position=(0, 0)):
         """
         Initialize an instance of Square.
@@ -17,12 +19,28 @@ class Square():
         with a private instance attribute size
         """
         self._Square__size = size
-        self._Square__position = position
+        if isinstance(position, tuple):
+            try:
+                if (isinstance(position[0], int) and
+                   isinstance(position[1], int)):
+                    if position[0] >= 0 and position[1] >= 0:
+                        self._Square__position = position
+                    else:
+                        raise ValueError(Square.TUPLEERROR)
+            except IndexError:
+                raise IndexError(Square.TUPLEERROR)
+        else:
+            raise TypeError(Square.TUPLEERROR)
 
     @property
     def size(self):
         """Return the size of the Square."""
         return self._Square__size
+
+    @property
+    def position(self):
+        """Return the position of the Square."""
+        return self._Square__position
 
     @size.setter
     def size(self, value):
@@ -35,22 +53,21 @@ class Square():
         else:
             raise TypeError("size must be an integer")
 
-    @property
-    def position(self):
-        """Return the position of the Square."""
-        return self._Square__position
-
     @position.setter
     def position(self, value):
         """Set the position of the Square."""
         if isinstance(value, tuple):
-            if value[0] >= 0 and value[1] >= 0:
-                self._Square__position = value
-            else:
-                raise ValueError("""position must be a tuple
-                                    of 2 positive integers""")
+            try:
+                if (isinstance(value[0], int) and
+                   isinstance(value[1], int)):
+                    if value[0] >= 0 and value[1] >= 0:
+                        self._Square__position = value
+                    else:
+                        raise ValueError(Square.TUPLEERROR)
+            except IndexError:
+                raise IndexError(Square.TUPLEERROR)
         else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError(Square.TUPLEERROR)
 
     def area(self):
         """Return the area of the Square."""
