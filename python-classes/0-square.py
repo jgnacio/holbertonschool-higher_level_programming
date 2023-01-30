9944dd5a3d5e1d5b27d28080d7a9e6a0dc61ecfd
jgnacio <jgnaciogomez@gmail.com>
2023-01-30 11:49:03 -0300

    add 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 11:46:00 2021.
+
+@author: jgnacio
+@description:
+
+"""
+
+class Square:
+    """Define an empty Square class."""
+
+    pass
