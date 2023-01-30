c0f369160ca6ddb57e79c484ee90a934f33d4eb6
jgnacio <jgnaciogomez@gmail.com>
2023-01-30 13:22:41 -0300

    add 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 13:04:00 2023.
+
+@author: jgnacio
+"""
+
+
+class Square():
+    """Represent a Square with size."""
+
+    def __init__(self, size=0):
+        """
+        Initialize an instance of Square.
+
+        with a private instance attribute size
+        """
+        self._Square__size = size
+
+    @property
+    def size(self):
+        """Return the size of the Square."""
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square."""
+        if isinstance(value, int):
+            if value >= 0:
+                self._Square__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Return the area of the Square."""
+        return self._Square__size ** 2
