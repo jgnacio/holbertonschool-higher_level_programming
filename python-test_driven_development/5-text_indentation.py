6b90d4a565fa3d9f751e08e9bfc31ac8ccf9eb4a
jgnacio <jgnaciogomez406@gmail.com>
2023-02-01 06:24:29 -0800

    Add 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,13 +7,27 @@ Created on Wed Feb 1 09:48:00 2023.
 @description:
 This module have a function:
     text_indentation (text) - that prints
-    the size of the square
+    print the indentation but replace all characters
+    after . ? and : with new lines.
 """
 
 
 def text_indentation(text):
-    """Print the indentation of the given text separated by . ? and : ."""
+    """Print the indentation of the given text indented after . ? and : ."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    s = text.replace(". ", "\n\n").replace("? ", "\n\n").replace(": ", "\n\n")
-    print(s)
+    matches = [".", "?", "."]
+    index = 0
+    while index < len(text):
+        if text[index] in matches:
+            print(f"{text[index]}\n\n", end="")
+            try:
+                if text[index + 1] == " ":
+                    index += 2
+                else:
+                    index += 1
+            except IndexError:
+                pass
+        else:
+            print(text[index], end="")
+            index += 1
