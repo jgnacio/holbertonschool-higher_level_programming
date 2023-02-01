bc4eca4bf86d7b259da63bd90293b8dcb008c309
jgnacio <jgnaciogomez406@gmail.com>
2023-02-01 03:56:19 -0800

    Create another list and not modify the origial one

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,25 +8,30 @@ This module have a function:
     matrix_divided (matrix, div) - that divide the matrix
 """
 
+
 def matrix_divided(matrix, div):
     """Divides the matrix."""
+    MATRIXERR = "matrix must be a matrix (list of lists) of integers/floats"
     if (not isinstance(matrix, list)
-        or not all(isinstance(elem, list) for elem in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+       or not all(isinstance(elem, list) for elem in matrix)):
+        raise TypeError(MATRIXERR)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = matrix[:]
-    len_row = len(new_matrix[0])
-    for i, elem in enumerate(new_matrix):
+    new_matrix = []
+    buffer_list = []
+    len_row = len(matrix[0])
+    for elem in matrix:
         if len_row != len(elem):
             raise TypeError("Each row of the matrix must have the same size")
-        for j, num in enumerate(elem):
+        for num in elem:
             if isinstance(num, (int, float)):
-                new_matrix[i][j] = round(num / div, 2)
+                buffer_list.append(round(num / div, 2))
             else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(MATRIXERR)
+        new_matrix.append(buffer_list)
+        buffer_list = []
 
     return new_matrix
