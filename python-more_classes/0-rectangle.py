eaefc48463645902119f65d0fe91478220e481da
jgnacio <jgnaciogomez406@gmail.com>
2023-02-02 09:05:22 -0800

    add base files

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 13:58:00 2023.
+
+@author: jgnacio
+@classes:
+Rectangle:
+    In this class is for representing an empty rectangle.
+"""
+
+
+class Rectangle:
+    """Empty class for Rectangle."""
+
+    pass
