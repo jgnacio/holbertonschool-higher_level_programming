28ea9cf78c10e59b27cf04a5b29d1bf65d45388a
jgnacio <jgnaciogomez406@gmail.com>
2023-02-03 04:35:04 -0800

    fix zero case

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -60,4 +60,6 @@ class Rectangle:
 
     def perimeter(self):
         """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width * 2) + (self.__height * 2)
