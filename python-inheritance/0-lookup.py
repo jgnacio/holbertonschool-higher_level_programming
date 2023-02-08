6896161371e85682d3846471e2271854f79d26ae
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 05:32:43 -0800

    add base files

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Feb 8 10:26:00 2023.
+
+@author: jgnacio
+@description:
+
+"""
+
+
+def lookup(obj):
+    """Lookup an object attributes and methods."""
+    return dir(obj)
