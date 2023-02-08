b069d68c0af88c935bc0e499534fdbd73440def7
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 07:22:59 -0800

    add 2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Feb 8 12:13:00 2023.
+
+@author: jgnacio
+@description:
+
+"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) is a_class:
+        return True
+    return False
