6eb3efc179367976480fc767102c24d3758dfaee
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 07:09:55 -0800

    Update files 1-

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -13,4 +13,6 @@ class MyList(list):
     """Print a list in order."""
 
     def print_sorted(self):
+        if hasattr(self, '__str__'):
             print(sorted(self))
+            return sorted(self)
