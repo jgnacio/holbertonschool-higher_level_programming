0fbc97b90a321942d6c43a79f07d72ee88b761b0
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 07:41:59 -0800

    add 4-inherit_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Feb 8 12:13:00 2023.
+
+@author: jgnacio
+@description:
+
+"""
+
+
+def inherits_from(obj, a_class):
+    return not type(obj) is a_class
