b53d1c8969631f47f1b7d0e13dd3fd841299317e
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 07:42:55 -0800

    add documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,5 @@ Created on Wen Feb 8 12:13:00 2023.
 
 
 def inherits_from(obj, a_class):
+    """Function that verify that a class inherits from a given class."""
     return not type(obj) is a_class
