edd1868083c233e53d836216c21e75b31a5e8679
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 07:24:46 -0800

    add documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -10,6 +10,7 @@ Created on Wen Feb 8 12:13:00 2023.
 
 
 def is_same_class(obj, a_class):
+    """Retrun if obj is instance of a class."""
     if type(obj) is a_class:
         return True
     return False
