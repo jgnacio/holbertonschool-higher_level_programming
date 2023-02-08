c397e84abc1653cfaf99344f318419f16270e981
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 11:24:12 -0800

    fix if statement

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Verify that the value is an integer."""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
