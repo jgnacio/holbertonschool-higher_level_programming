af2eec709b916ba71866b73512dedfcafd95f3b8
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 08:55:13 -0800

    fix if statement

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ Created on Wen Feb 8 12:13:00 2023.
 
 def inherits_from(obj, a_class):
     """Function that verify that a class inherits from a given class."""
-    if type(obj) is a_class:
+    if type(obj) == a_class:
         return False
 
     return issubclass(a_class, type(obj))
