67f2446f5aa6c01347895d34c57f53b23beba010
jgnacio <jgnaciogomez406@gmail.com>
2023-02-08 09:05:45 -0800

    add 5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Feb 8 12:13:00 2023.
+
+@author: jgnacio
+@description:
+
+"""
+
+
+class BaseGeometry:
+    """Base empty class."""
+
+    pass
