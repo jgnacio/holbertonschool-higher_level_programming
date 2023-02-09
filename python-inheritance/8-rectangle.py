53770c4c1034e3e38fda782281435455ad816464
jgnacio <jgnaciogomez406@gmail.com>
2023-02-09 08:42:18 -0800

    add 8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Feb 8 15:41:00 2023.
+
+@author: jgnacio
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class representing a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle."""
+        self.integer_validator(f"{width}", width)
+        self.integer_validator(f"{height}", height)
+        self.__width = width
+        self.__height = height
