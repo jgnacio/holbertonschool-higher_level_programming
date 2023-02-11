0346aee2fdbaeac028ef3ef1c1262f4ab719d69e
jgnacio <jgnaciogomez@gmail.com>
2023-02-10 21:44:02 -0300

    not allowed to use import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,9 +8,7 @@ Created on Fri Feb 10 20:29:00 2023.
 
 """
 
-import json
-
 
 def class_to_json(obj):
     """Return JSON string from object class."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
