e99c127a4d8bab709612649b48b9b8bb11d4a080
jgnacio <jgnaciogomez@gmail.com>
2023-02-10 21:20:23 -0300

    add 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 10 20:27:00 2023.
+Created on Fri Feb 10 20:36:00 2023.
 
 @author: jgnacio
 @description:
@@ -9,8 +9,16 @@ Created on Fri Feb 10 20:27:00 2023.
 """
 
 import sys
+import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-with open("add_item.json", "a") as f:
-    f.write(str(sys.argv))
+try:
+    jsonobj = load_from_json_file('add_item.json')
+except Exception:
+    jsonobj = []
+
+for arg in sys.argv[1:]:
+    jsonobj.append(arg)
+
+save_to_json_file(jsonobj, "add_item.json")
