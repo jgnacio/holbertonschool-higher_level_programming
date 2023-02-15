abc41c666ba047840db18bf3f494b2796e87c336
jgnacio <jgnaciogomez@gmail.com>
2023-02-15 12:04:48 -0300

    Fix None case

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,5 +28,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """Convert a list of dictionaries to a json string."""
         if list_dictionaries is None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
