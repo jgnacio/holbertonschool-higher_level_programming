4f49af910a00ef8864d2752e5a3ca6df9762ebe1
jgnacio <jgnaciogomez@gmail.com>
2023-02-16 16:22:10 -0300

    Update tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -5,7 +5,7 @@ from models import Base
 
 class Tests(unittest.TestCase):
 
-    def test_base(self):
+    def test_id(self):
         """Test max integer function."""
         test = Base()
         self.assertEqual(test.id, 1)
@@ -13,8 +13,20 @@ class Tests(unittest.TestCase):
         self.assertEqual(test.id, 12)
         test = Base()
         self.assertEqual(test.id, 2)
-        test = Base(44)
-        self.assertEqual(test.id, 44)
+        test = Base(-44)
+        self.assertEqual(test.id, -44)
+
+    def test_to_json_string(self):
+        self.assertAlmostEqual(Base.to_json_string(None), "[]")
+        self.assertAlmostEqual(Base.to_json_string([]), "[]")
+        test = Base.to_json_string([{"id": 1}])
+        self.assertAlmostEqual(test, '[{"id": 1}]')
+
+    def test_from_json_string(self):
+        self.assertAlmostEqual(Base.from_json_string(None), [])
+        self.assertAlmostEqual(Base.from_json_string("[]"), [])
+        test = Base.from_json_string('[{ "id": 89 }]')
+        self.assertAlmostEqual(test, [{"id": 89}])
 
 
 if __name__ == '__main__':
