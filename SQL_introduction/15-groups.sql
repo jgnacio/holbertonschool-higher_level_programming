8100189644616776e7591046cbb139c0e90ec2d9
jgnacio <jgnaciogomez406@gmail.com>
2023-03-13 14:05:15 -0700

    fix NUMBER to number, and order by score desc.

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Script that lists the number of records with
 -- the same score in the table second_table.
-SELECT score, COUNT(*) AS NUMBER FROM second_table GROUP BY score;
+SELECT score, count(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
