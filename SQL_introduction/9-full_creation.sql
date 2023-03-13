7bfda0905c3e1eefb348aa61a61b3321600f407e
jgnacio <jgnaciogomez406@gmail.com>
2023-03-13 12:58:55 -0700

    Add 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+--
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(255),
+    score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score)
+VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score)
+VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score)
+VALUES (4, 'George', 8);
