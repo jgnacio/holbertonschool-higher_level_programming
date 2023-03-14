2bbc68243d5e00903aa828257c6c3ffe75dd603c
jgnacio <jgnaciogomez406@gmail.com>
2023-03-14 06:57:29 -0700

    Remove charset modification

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Script that converts hbtn_0c_0 database to UTF8.
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(255) COLLATE utf8mb4_unicode_ci;
