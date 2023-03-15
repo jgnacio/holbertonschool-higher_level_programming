d673968ecac13de80ef7a435d69128c96cf5febd
jgnacio <jgnaciogomez406@gmail.com>
2023-03-15 11:13:28 -0700

    Add 9-cities_by_state_join.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = state_id ORDER BY cities.id;
