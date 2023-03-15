8e5b0c4a6c2c49690faa7c8ec8a86e49251a149f
jgnacio <jgnaciogomez406@gmail.com>
2023-03-15 10:01:13 -0700

    Fix not null id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Script that creates the table unique_id.
-CREATE TABLE IF NOT EXISTS unique_id (id INT(1) UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT(1) PRIMARY KEY, name VARCHAR(256));
