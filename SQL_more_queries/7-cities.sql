aa7d78e367ddf0f2cc4b863c9d819b6cbab5133a
jgnacio <jgnaciogomez406@gmail.com>
2023-03-15 10:38:36 -0700

    Rename states_id to state_id

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- states_id references to id in the tables states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE cities (id INT PRIMARY KEY AUTO_INCREMENT, states_id INT, name VARCHAR(256) NOT NULL, FOREIGN KEY (states_id) REFERENCES states(id));
+CREATE TABLE cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
