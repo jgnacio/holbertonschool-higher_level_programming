9252c9a175831df9c83ce943b7e006e540cd128f
jgnacio <jgnaciogomez406@gmail.com>
2023-03-16 11:29:05 -0700

    Put outside of the function

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 Created on Thur March 16 02:37:00 2023.
 
@@ -11,37 +12,30 @@ import sys
 import MySQLdb
 
 
-def conect_to_db():
-    """Make Connection to MySQL database, and list their sontent."""
-    # Trying to connect
-    try:
-        db_connection = MySQLdb.connect(
-            "localhost",
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3])
-    # If connection is not successful
-    except MySQLdb.OperationalError:
-        print("Can't connect to database")
-        return 0
+# Trying to connect
+try:
+    db_connection = MySQLdb.connect(
+        "localhost",
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3])
+# If connection is not successful
+except MySQLdb.OperationalError:
+    print("Can't connect to database")
 
-    # Making Cursor Object For Query Execution
-    cursor = db_connection.cursor()
+# Making Cursor Object For Query Execution
+cursor = db_connection.cursor()
 
-    # Executing Query
-    cursor.execute("SELECT * FROM states")
+# Executing Query
+cursor.execute("SELECT * FROM states")
 
-    # Above Query Gives Us The Current Date
-    # Fetching all Data
-    states = cursor.fetchall()
+# Above Query Gives Us The Current Date
+# Fetching all Data
+states = cursor.fetchall()
 
-    # Printing Rresult Of Above
-    for state in states:
-        print(state)
+# Printing Rresult Of Above
+for state in states:
+    print(state)
 
-    # Closing Database Connection
-    db_connection.close()
-
-
-# Function Call For Connecting To Our Database
-conect_to_db()
+# Closing Database Connection
+db_connection.close()
