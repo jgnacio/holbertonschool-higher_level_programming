a2ebd739b9e949d81f90581a504a4b583b449b59
jgnacio <jgnaciogomez406@gmail.com>
2023-03-16 12:18:15 -0700

    Add 2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thur March 16 04:08:00 2023.
+
+@author: jgnacio
+@description:
+    This module provides a simple connection to
+    a Mysql database with MySQLdb module, using
+    the comand line arguments.
+
+    That lists all states with a name of the 5
+    argument passed.
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    # Trying to connect
+    try:
+        db_connection = MySQLdb.connect(
+            "localhost",
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3])
+        # If connection is not successful
+    except MySQLdb.OperationalError:
+        print("Can't connect to database")
+
+    # Making Cursor Object For Query Execution
+    cursor = db_connection.cursor()
+
+    # Executing Query
+    cursor.execute(
+        f"SELECT * FROM states where name = \"{sys.argv[4]}\" ORDER BY id"
+    )
+
+    # Above Query Gives Us The Current Date
+    # Fetching all Data
+    states = cursor.fetchall()
+
+    # Printing Rresult Of Above
+    for state in states:
+        print(state)
+
+    # Closing Database Connection
+    db_connection.close()
