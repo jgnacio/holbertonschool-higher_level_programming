5701cfbecb6b9e38d0618f3f34ba2b6f06db4073
jgnacio <jgnaciogomez@gmail.com>
2023-03-17 13:39:15 -0300

    Add an init statement on the state module

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -23,3 +23,6 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, autoincrement="auto", primary_key=True)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
