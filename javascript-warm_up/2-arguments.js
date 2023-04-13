7ce69288f90bb7468e91a366ecca41b2633eef8b
jgnacio <jgnaciogomez406@gmail.com>
2023-04-13 09:16:19 -0700

    semistandar fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
-argv.length > 1 ? console.log("Arguments found") : console.log("No argument");
+const { argv } = require('node:process');
+argv.length > 1 ? console.log('Arguments found') : console.log('No argument');
