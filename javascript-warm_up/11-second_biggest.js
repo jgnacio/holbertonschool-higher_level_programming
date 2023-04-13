9405b3711126345a5ceb9f3b53f839aabebe01b7
jgnacio <jgnaciogomez406@gmail.com>
2023-04-13 13:26:19 -0700

    Add 11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const bigest = [0, 0];
+args.forEach(element => {
+  if (+element > bigest[0]) {
+    if (bigest[0]) {
+      bigest[1] = bigest[0];
+    }
+    bigest[0] = +element;
+    console.log(bigest);
+  }
+});
+console.log(bigest[1]);
