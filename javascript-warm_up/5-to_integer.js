6f857f31a9875f2405a000d71bced7c1331a3199
jgnacio <jgnaciogomez406@gmail.com>
2023-04-13 10:08:32 -0700

    Fix semistandard validation and checking

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-+args[0] ?? undefined == undefined
++args[0] ?? undefined
   ? console.log(`My number: ${+args[0]}`)
-  : console.log("Not a number");
+  : console.log('Not a number');
