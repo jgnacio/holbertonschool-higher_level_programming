6e6cb0bb9378e512f2da175237c15e99e4356eee
jgnacio <jgnaciogomez406@gmail.com>
2023-04-13 09:48:39 -0700

    Dont use length check if undefined

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('No argument');
-} else if (args.length === 1) {
+if (args[0] !== undefined) {
   console.log(args[0]);
+} else {
+  console.log('No argument');
 }
