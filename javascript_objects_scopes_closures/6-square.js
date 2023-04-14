0f6977564dcca3ab8f2c469b366e431edd131905
jgnacio <jgnaciogomez406@gmail.com>
2023-04-14 07:33:15 -0700

    Remove useless constructor

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const Rectangle = require('./5-square');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) c = 'X';
     for (let i = 0; i < this.width; i++) {
