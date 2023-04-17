5847c8fc195f3c025058797f0f84e3dd1bc1700d
jgnacio <jgnaciogomez406@gmail.com>
2023-04-17 06:57:46 -0700

    Give the url request on args

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
 const request = require('request');
 
-request.get(
-  'https://swapi-api.alx-tools.com/api/films/',
-  (error, response, body) => {
-    if (error) {
-      console.log(error);
-    }
-    const films = JSON.parse(body);
-    let countOfAntilles = 0;
-    if (films) {
-      for (const film of films.results) {
-        for (const character of film.characters) {
-          if (character.split('/').slice(5, 6)[0] === '18') {
-            countOfAntilles += 1;
-          }
+request.get(args[0], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const films = JSON.parse(body);
+  let countOfAntilles = 0;
+  if (films) {
+    for (const film of films.results) {
+      for (const character of film.characters) {
+        if (character.split('/').slice(5, 6)[0] === '18') {
+          countOfAntilles += 1;
         }
       }
-      console.log(countOfAntilles);
     }
+    console.log(countOfAntilles);
   }
-);
+});
