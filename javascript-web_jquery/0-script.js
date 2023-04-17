4f3e0c066fdd9bbc4ea8697ff625e869044c67d4
jgnacio <jgnaciogomez406@gmail.com>
2023-04-17 09:21:43 -0700

    Add 0-script.js

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,4 +1,2 @@
-window.onload(() => {
-  const header = document.querySelector('header');
-  if (header) header.style.color = '#FF0000';
-});
+const header = document.querySelector('header');
+if (header) header.style.color = '#FF0000';
