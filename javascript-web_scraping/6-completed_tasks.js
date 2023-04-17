8c52055043180f13ed6a3ea5743184f3844c44d0
jgnacio <jgnaciogomez406@gmail.com>
2023-04-17 07:59:44 -0700

    Fix empty records and not sorted json id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,22 +7,22 @@ request.get(args[0], (error, response, body) => {
     console.log(error);
   } else {
     const users = JSON.parse(body);
-    // Obj to store the count of task completed for each user
-    const countTask = {};
-    /* This array counts in the fist index the id of the user,
-     and the second contains the number of task completed */
-    const currentCount = [0, 0];
-    let user;
-    for (user of users) {
-      /* If the current count not is the same as the user id reset
-       the current count */
-      if (currentCount[0] !== user.userId) {
-        currentCount[0] = user.userId;
-        currentCount[1] = 0;
-      }
-      if (user.completed) currentCount[1] += 1;
-      countTask[user.userId] = currentCount[1];
-    }
+    const countTask = countTaskFunction(users);
     console.log(countTask);
   }
 });
+
+function countTaskFunction (users) {
+  // Obj to store the count of task completed for each user
+  const countTask = {};
+  let user;
+  for (user of users) {
+    if (user.completed === true) {
+      /* if user already have a record in the object,
+        increment by one the count, else set to one. */
+      if (countTask[user.userId]) countTask[user.userId] += 1;
+      else countTask[user.userId] = 1;
+    }
+  }
+  return countTask;
+}
