bc0bcb144d9366adbcec4391c1749e7c73d3679a
jgnacio <jgnaciogomez406@gmail.com>
2023-04-17 06:02:12 -0700

    Add 2-statuscode.js

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/user/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+
+request(args[0], (error, response, body) => {
+  if (response) console.log(`code: ${response.statusCode}`);
+  else console.log(`code: ${error.code}`);
+});
